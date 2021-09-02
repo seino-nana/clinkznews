78720d3e14e73adfa44f040bfdccd43d6d947c5b
pugna <674408526@qq.com>
2021-09-02 17:11:21 +0800

    bug解决

---
+++ src/components/content/news/NewListItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="news-list-item">
+    <a :href="item.url">
+      <div class="news-info">
+        <div class="title">{{item.title}}</div>
+        <span class="ctime">{{item.ctime}}</span>
+        <span class="source">{{item.source}}</span>
+      </div>
+      <img v-lazy="item.picUrl" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewsListItem',
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.news-list-item a {
+  position: relative;
+  display: flex;
+  padding: 5px;
+  overflow: hidden;
+}
+.news-info {
+  width: 220px;
+}
+.title {
+    padding-top: 5px;
+}
+.ctime,.source {
+  font-size: 14px;
+}
+.ctime {
+  position: absolute;
+  left: 5px;
+  bottom: 0;
+  font-size: 14px;
+}
+.source {
+  position: absolute;
+  left: 5px;
+  bottom: 18px;
+  
+}
+
+.news-list-item img{
+  flex: 1;
+  width: 80px;
+  height: 110px;
+  padding: 5px;
+  border-radius: 12px;
+}
+
+</style>
---
+++ src/components/content/news/NewsList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="news-list">
+      <news-list-item class="news-list-item" v-for="item in news" :key="item.index" :item="item"/>  
+    </div>  
+</template>
+
+<script>
+import NewsListItem from './NewListItem.vue'
+
+export default {
+  name: 'GoodsList',
+  props: {
+    news: {
+      type: Array,
+      default() {
+         return []
+      }
+    }
+  },
+  components: {
+    NewsListItem
+  }
+}
+</script>
+
+<style scoped>
+.news-list-item {
+  height: 120px;
+  margin: 5px;
+  background-color: #fff;
+  border-radius: 10px;
+}
+</style>
